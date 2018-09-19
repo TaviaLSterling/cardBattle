ad89362bd636197ce8d38c3a149be86549af89f1
Tavia Sterling <TaviaSterling@outlook.com>
2018-09-19 15:24:17 -0600

    deploy project

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -1,57 +1,60 @@
 <template>
   <div class="game container-fluid">
-   
-    <player :fnclick="setPlayerCard"></player>
-    <button class="btn btn-success" @click="attack">Attack!</button>
-    <opponent :fnclick="setOpponentCard"></opponent>
-    
-      </div>
+    <div v-if="!activeGame.winner">
+      <player :fnclick="setPlayerCard"></player>
+      <button class="btn btn-success" @click="attack">Attack!</button>
+      <opponent :fnclick="setOpponentCard"></opponent>
+    </div>
+    <div v-else>
+      {{activeGame.winner}} WINS!
+    </div>
+  </div>
 </template>
 
 <script>
-import Player from "../components/Player"
-import Opponent from "../components/Opponent"
+  import Player from "../components/Player"
+  import Opponent from "../components/Opponent"
 
-export default { 
-  name: "game", 
-  data() {   
-    return {
-attackObject : {
-  playerId: "",
-	playerCardId: "",
-	opponentId: "",
-  opponentCardId: "",
-  gameId: ''
-}
-}
-},
- 
-  computed:{
-   activeGame() {
-     return this.$store.state.activeGame;
-   }
- },
- methods: {
-   attack() {
-     this.attackObject.gameId = this.activeGame.id
-     this.$store.dispatch('attack',this.attackObject)
-   },
-   setPlayerCard(playerId, playerCardId) {
-     this.attackObject.playerId = playerId
-     this.attackObject.playerCardId = playerCardId
-   },
-   setOpponentCard(opponentId,opponentCardId) {
-     this.attackObject.opponentId = opponentId
-     this.attackObject.opponentCardId = opponentCardId
-   }
- },
-   components: {
-     'player': Player,
-    'opponent': Opponent
-   }
- 
- }
-  
+  export default {
+    name: "game",
+    data() {
+      return {
+        attackObject: {
+          playerId: "",
+          playerCardId: "",
+          opponentId: "",
+          opponentCardId: "",
+          gameId: ''
+        }
+      }
+    },
+
+    computed: {
+      activeGame() {
+        return this.$store.state.activeGame;
+      }
+    },
+    methods: {
+      attack() {
+        this.attackObject.gameId = this.activeGame.id
+        this.$store.dispatch('attack', this.attackObject)
+      },
+      setPlayerCard(playerId, playerCardId) {
+        this.attackObject.playerId = playerId
+        this.attackObject.playerCardId = playerCardId
+      },
+      setOpponentCard(opponentId, opponentCardId) {
+        this.attackObject.opponentId = opponentId
+        this.attackObject.opponentCardId = opponentCardId
+      }
+    },
+    components: {
+      'player': Player,
+      'opponent': Opponent
+    }
+
+  }
+
 
 
 
@@ -68,11 +71,10 @@ attackObject : {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
-.game {
-  background-image:url('../assets/galaxy.jpg');
-  background-size: cover;
-  color:white;
-  font-family: 'Permanent Marker', cursive;
-}
+  .game {
+    background-image: url('../assets/galaxy.jpg');
+    background-size: cover;
+    color: white;
+    font-family: 'Permanent Marker', cursive;
+  }
 </style>
